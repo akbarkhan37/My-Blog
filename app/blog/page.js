27ca009fbed690f9dee9c13cd3d1fc819d6fc0ff
Akbar Khan <akbarkhan37@gmail.com>
2025-12-02 16:36:00 +0530

    Revert "txt"
    This reverts commit 0a56edad576a1842559c4b08e57db79c2a27dd31.

--- app/blog/page.js
+++ app/blog/page.js
@@ -26,7 +26,6 @@ export default async function BlogList() {
             className="text-blue-600 hover:text-blue-800 underline">{post.title}</Link>
           </li>
         ))}
-        <h1>testingg</h1>
       </ul>
     </div>
   );
